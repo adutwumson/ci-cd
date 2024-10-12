cc6d8318e8a7ca453b8d19bda1f87da97a596757
adutwumson <braimah_abbass.yahoo.com>
2024-10-12 09:30:27 +0100

    first commit of project

---
+++ src/test/java/com/seiduabbas/DevOpsMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seiduabbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
